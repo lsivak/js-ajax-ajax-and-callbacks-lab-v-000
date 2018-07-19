6b47c23a920045f54cccba32e876d10066f43fbe
Lara Sanders <larasanders@gmail.com>
2018-07-19 17:54:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
-var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
 
-var renderCommit = (commit) => {
-  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
-}
-
-var renderCommits = (data) => {
-  let result = data.map((commit)=>renderCommit(commit)).join('')
-  return `<ul>${result}</ul>`
-}
-
-var showCommits = (el) => {
-  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
-    $('#details').html(renderCommits(data))
-  }).fail(error => {
-    displayError()
-  })
-}
-
-var renderSearchResult = (result) => {
-  return `
-      <div>
-        <h2><a href="${result.html_url}">${result.name}</a></h2>
-        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-        <p>${result.description}</p>
-      </div>
-      <hr>
-    `
-}
-
-var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
-
-var searchRepositories = () => {
-  const searchTerms = $('#searchTerms').val()
-  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
-      $('#results').html(renderSearchResults(data))
-    }).fail(error => {
-      displayError()
-    })
-}
-
+const searchTerms = $('#searchTerms')
 $(document).ready(function (){
+$.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function( results ) {
+  $( "#searchTerms" ).html( results );
+  alert( "Load was performed." );
 });
+$.get("error", function(data) {
+// This will not be called because the .html file request doesn't exist
+displayError();
+}).fail(function(error) {
+// This is called when an error occurs
+console.log("I'm sorry, there's been an error. Please try again.");
+});
+})
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+   const repoList = `<ul>${repos.map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a> <a href="#" data-repository="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
