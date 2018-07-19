e3fa279ca2b1ce16006ea3c88a64f16f046ba2fa
Lara Sanders <larasanders@gmail.com>
2018-07-19 14:51:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-$(document).ready(function (){
+$.get( "https://api.github.com/", function( results ) {
+  $( ".searchTerms" ).html( results );
+  alert( "Load was performed." );
+});
+$.get("this_doesnt_exist.html", function(data) {
+// This will not be called because the .html file request doesn't exist
+displayError();
+}).fail(function(error) {
+// This is called when an error occurs
+console.log("I'm sorry, there's been an error. Please try again.");
 });
