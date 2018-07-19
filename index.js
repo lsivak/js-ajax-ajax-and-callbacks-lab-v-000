0ebb6d01f79ff76c5f998e05193c0a732668e3ee
Lara Sanders <larasanders@gmail.com>
2018-07-19 18:27:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,16 +24,18 @@ function resultsData() {
   document.getElementById("repositories").innerHTML = repoList
 }
 
-function getCommits(el) {
-  const name = el.dataset.repository
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showCommits)
-  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
-  req.send()
+function showCommits(el) {
+   $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function (results) => {
+     $( "#details" ).html( results );
+     displayError();
+     }).fail(function(error) {
+     // This is called when an error occurs
+     console.log("I'm sorry, there's been an error. Please try again.");
+     });
+     })
 
-}
 
-function showCommits() {
+function displayCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.commit.author.name + '</strong> - '  +  commit.author.login  + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
