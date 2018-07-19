fcd54c11112d07f26b4984e6249cb551ce890f92
Lara Sanders <larasanders@gmail.com>
2018-07-19 17:47:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+
+
 $(document).ready(function (){
 $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function( results ) {
-  $( ".searchTerms" ).html( results );
+  $( "#searchTerms" ).html( results );
   alert( "Load was performed." );
 });
 $.get("error", function(data) {
