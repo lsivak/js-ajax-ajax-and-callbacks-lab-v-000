a68350b6399a9080ac017a5cca3100aab789d214
Lara Sanders <larasanders@gmail.com>
2018-07-19 17:45:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-$.get( "https://api.github.com/repos/octocat/", function( results ) {
+$.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function( results ) {
   $( ".searchTerms" ).html( results );
   alert( "Load was performed." );
 });
