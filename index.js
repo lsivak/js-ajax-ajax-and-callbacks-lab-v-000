c2457e5ed34675d46c70e128718cc4e1c57a44e7
Lara Sanders <larasanders@gmail.com>
2018-07-19 17:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const searchRepositories = () => {
-const searchTerms = $('#searchTerms')
+const searchTerms = $('#searchTerms').val()
 $(document).ready(function (){
 $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function( results ) {
   $( "#searchTerms" ).html( results );
